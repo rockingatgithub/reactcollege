0b166374380c75774e61268c0aca3be7cc073f4b
rockingatgithub <contactwithsid@gmail.com>
2020-09-30 15:10:16 +0530

    Updates

---
+++ precache-manifest.ab41806e0850cb6d19668240253da590.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac0627a76833b5b3c916989bb92d3f4",
+    "url": "/reactcollege/index.html"
+  },
+  {
+    "revision": "03ac53f0cc12156bba52",
+    "url": "/reactcollege/static/css/main.1c521d3a.chunk.css"
+  },
+  {
+    "revision": "dd9693396250523cfcce",
+    "url": "/reactcollege/static/js/2.04af431d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcollege/static/js/2.04af431d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ac53f0cc12156bba52",
+    "url": "/reactcollege/static/js/main.b7722b2a.chunk.js"
+  },
+  {
+    "revision": "6204a162896ccf9181d1",
+    "url": "/reactcollege/static/js/runtime-main.9b41c5cb.js"
+  },
+  {
+    "revision": "8793ec0ee96ac6622dd5064036daac2a",
+    "url": "/reactcollege/static/media/college_01.8793ec0e.jpg"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/reactcollege/static/media/college_02.619ee97e.jpg"
+  },
+  {
+    "revision": "43ead0b4d389002da6529350eed262fb",
+    "url": "/reactcollege/static/media/disccount.43ead0b4.svg"
+  },
+  {
+    "revision": "82d43860224b8b900350131f9a9b8be1",
+    "url": "/reactcollege/static/media/promotedtext.82d43860.svg"
+  },
+  {
+    "revision": "b81fbf78529a897b0c5b6fdad1d8e52e",
+    "url": "/reactcollege/static/media/ratingstars.b81fbf78.PNG"
+  }
+]);
